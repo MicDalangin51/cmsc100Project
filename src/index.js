17467d3b3ad5456ce4b8b36eddfc43893283268d
Martin Ivan Dalangin <mic@martins-mbp.lan>
2023-01-06 15:32:31 +0800

    chore: install semistandard and @fastify/view in our dev dependencies

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-import Fastify from 'fastify'
+import { build } from './app.js';
 
-async function start() {
-    try {
-        //This would initialize fastify
-        const fastify = Fastify({logger: true});
+async function start () {
+  try {
+    // This would initialize fastify
+    const fastify = await build();
 
-        fastify.get('/api', async (request,reply) => {
-            return { success:true };
-        });
+    const addr = fastify.listen({
+      port: '8080'
 
-        const addr = fastify.listen({
-            port: '8080',
+    });
 
-        });
-
-        console.log(`Listening on port ${addr}`);
-    } catch(error) {   //This executes if an error is encountered in the 'try' part
-        //prints the error
-        console.error(error);
-        //exits the program
-        process.exit(1);
-    }
+    console.log(`Listening on port ${addr}`);
+  } catch (error) { // This executes if an error is encountered in the 'try' part
+    // prints the error
+    console.error(error);
+    // exits the program
+    process.exit(1);
+  }
 }
 
 start();
