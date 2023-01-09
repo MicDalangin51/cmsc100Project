b43d9507a2b51a50c9b14dc1ab2f6adfe285a7da
Martin Ivan Dalangin <mic@martins-mbp.lan>
2023-01-10 04:24:20 +0800

    feat: create mechanism and path for getting a specific blog post

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ejs from 'ejs';
 import fastifyView from '@fastify/view';
 import { createBlogPage } from './services/blog-page-services/create-blog-page.js';
 import { listBlogPage } from './services/blog-page-services/list-blog-page.js';
+import { getBlogPost } from './services/blog-page-services/get-blog.js';
 import { general } from './services/general/index.js';
 
 const prefix = '/api';
@@ -23,6 +24,9 @@ export async function build () {
 
   // This would be the route for list blog page
   fastify.get(`${prefix}/blog`, listBlogPage);
+
+  // This woulf be the route for getting a specific blog post
+  fastify.get(`${prefix}/blog/:blogId`,getBlogPost);
 
   return fastify;
 }
